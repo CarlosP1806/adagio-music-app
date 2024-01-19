ed0a3615a6f75d2eacd808af4a23582ac0f1e8f7
Carlos Paez <carlosalbertopaez2002@gmail.com>
2024-01-18 18:50:06 -0600

    Add session model and refactor services

---
+++ server/prisma/migrations/20240119002340_add_note_model_and_scores_to_practice_session/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `satisfaction` on the `PracticeSession` table. All the data in the column will be lost.
+  - Added the required column `comfortScore` to the `PracticeSession` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `difficultyScore` to the `PracticeSession` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `duration` to the `PracticeSession` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `musicalityScore` to the `PracticeSession` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `satisfactionScore` to the `PracticeSession` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `techniqueScore` to the `PracticeSession` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `PracticeSession` DROP COLUMN `satisfaction`,
+    ADD COLUMN `comfortScore` INTEGER NOT NULL,
+    ADD COLUMN `difficultyScore` INTEGER NOT NULL,
+    ADD COLUMN `duration` INTEGER NOT NULL,
+    ADD COLUMN `musicalityScore` INTEGER NOT NULL,
+    ADD COLUMN `satisfactionScore` INTEGER NOT NULL,
+    ADD COLUMN `techniqueScore` INTEGER NOT NULL;
+
+-- CreateTable
+CREATE TABLE `Note` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `practiceId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Note` ADD CONSTRAINT `Note_practiceId_fkey` FOREIGN KEY (`practiceId`) REFERENCES `PracticeSession`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- server/src/services/practiceSession.service.ts
+++ server/src/services/practiceSession.service.ts
@@ -1,33 +1,27 @@
 import type { PracticeSession } from "../constants/types";
 import { db } from "../db/db.server";
 
-// Returns a list of all practice sessions
+// Return a list of all practice sessions
 export const getAllSessions = async (): Promise<PracticeSession[]> => {
-  return db.practiceSession.findMany({
-    select: {
-      id: true,
-      startTime: true,
-      endTime: true,
-      satisfaction: true,
-      userId: true,
-    },
+  return db.practiceSession.findMany();
+};
+
+// Create a new practice session
+export const createSession = async (
+  session: PracticeSession
+): Promise<PracticeSession> => {
+  return db.practiceSession.create({
+    data: session,
   });
 };
 
-// Returns list of sessions by user
+// Return list of sessions by user
 export const getSessionsByUser = async (
   userId: number
 ): Promise<PracticeSession[]> => {
   return db.practiceSession.findMany({
     where: {
       userId: userId,
-    },
-    select: {
-      id: true,
-      startTime: true,
-      endTime: true,
-      satisfaction: true,
-      userId: true,
     },
   });
 };
