b43c64537d3f4ba7bd3af3dff8326d15f3069717
Carlos Paez <carlosalbertopaez2002@gmail.com>
2024-01-27 21:34:51 -0600

    Refactor types

--- client/src/features/recording/services/recording.services.ts
+++ client/src/features/recording/services/recording.services.ts
@@ -1,5 +1,5 @@
 import { baseApi } from "../../../services/baseApi";
 
-export function uploadToCloud(recData: any) {
+export function uploadToCloud(recData: FormData) {
   return baseApi.post("/recording/upload", recData).then((res) => res.data);
 }
